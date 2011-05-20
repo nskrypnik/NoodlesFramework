0ee3ec2fef3e54b48c850f247fa288efb067ed21
Niko Skrypnik <nskrypnik@gmail.com>
2011-05-20 22:21:44 +0300

    Implement data saving to Redis in datastorage

--- datastore.py
+++ datastore.py
@@ -4,71 +4,7 @@ import redis
 
 non_record = re.compile(r'__\w+__')
 
-class Model(object):
-    " General class for objects description in data storage "
-    # static model parameters
-    __structure__ = {}
-    __collection__ = {} # Collection name for Mongo DB
-    
-    def __init__(self, valuedict=None, embedded=False, **kwargs):
-        classname = self.__class__.__name__
-        self.__init_structure__(classname, valuedict, embedded, **kwargs)
-        
-        storage = kwargs.get('storage')
-        if not storage: self.storage = 'redis'
-        
-        self.id = None 
-        
-        
-    def __init_structure__(self, classname, valuedict=None, embedded=False, **kwargs):
-        # create dictionary for model instance
-        self.__instdict__ = {}
-        # Check static model structure
-        if not self.__structure__.get(classname):
-        # if not - create it
-            self.__structure__[classname] = {}
-            # Specify the collection name
-            self.__collection__[classname] = classname.lower() + 's'
-            logging.debug('Creating structure for model %s' % (classname))
-            # Browse model for properties
-            for key in dir(self):
-                if not non_record.match(key):
-                    value = getattr(self, key)
-                    if hasattr(value, '__isvalue__'):
-                        value.set_key(key)
-                        self.__structure__[classname][key] = value.get_default()
-        if valuedict:
-            self.__instdict__ = valuedict
-        else:
-            self.__instdict__ = copy.deepcopy(self.__structure__[classname])
-        
-        for k in kwargs:
-            if k in self.__instdict__:
-                if hasattr(kwargs[k], 'get_values'):
-                    self.__instdict__[k] = kwargs[k].get_values()
-                else:
-                    self.__instdict__[k] = kwargs[k]
-            else:
-                raise Exception('There is no such value \'%s\' in %s model.' % (k, classname))
-        
-    
-    def get_structure(self):
-        classname = self.__class__.__name__
-        return self.__structure__.get(classname)
-    
-    def get_collection_name(self):
-        classname = self.__class__.__name__
-        return self.__collection__[classname]
-    
-    def get_values(self):
-        return copy.deepcopy(self.__instdict__)
-        
-    def load(self):
-        pass
-    
-    def save(self):
-        pass
-                 
+REDIS_CONN = redis.Redis()
 
 class Value(object):
     "Single value in our data storage"
@@ -100,8 +36,102 @@ class Value(object):
         valuedict = instance.__instdict__
         valuedict[self.key] = self.type(value)
 
-class Index(Value):
-    pass
+
+class Model(object):
+    " General class for objects description in data storage "
+    # static model parameters
+    __structure__ = {}
+    __collection__ = {} # Collection name for Mongo DB
+    
+    id = Value(int)
+    
+    def __init__(self, valuedict=None, embedded=False, **kwargs):
+        classname = self.__class__.__name__
+        
+        self.__init_structure__(classname, valuedict, **kwargs)
+        
+        self.collection_name = self.__collection__[classname]
+        
+        storage = kwargs.get('storage')
+        if not storage: self.storage = 'redis'
+        
+        #self.id = None
+        self.embedded = embedded
+        self.save_routines = {'redis': self.save_redis, 'mongo': self.save_mongo}
+        
+    
+    def __init_structure__(self, classname, valuedict=None, **kwargs):
+        # create dictionary for model instance
+        self.__instdict__ = {}
+        # Check static model structure
+        if not self.__structure__.get(classname):
+        # if not - create it
+            self.__structure__[classname] = {}
+            # Specify the collection name
+            self.__collection__[classname] = classname.lower() + 's'
+            logging.debug('Creating structure for model %s' % (classname))
+            # Browse model for properties
+            for key in dir(self):
+                if not non_record.match(key):
+                    value = getattr(self, key)
+                    if hasattr(value, '__isvalue__'):
+                        value.set_key(key)
+                        self.__structure__[classname][key] = value.get_default()
+        if valuedict:
+            self.__instdict__ = valuedict
+        else:
+            self.__instdict__ = copy.deepcopy(self.__structure__[classname])
+        
+        for k in kwargs:
+            if k in self.__instdict__:
+                if hasattr(kwargs[k], 'get_values'):
+                    self.__instdict__[k] = kwargs[k].get_values()
+                else:
+                    self.__instdict__[k] = kwargs[k]
+            else:
+                raise Exception('There is no such value \'%s\' in %s model.' % (k, classname))
+    
+    def save(self, storage = None):
+        if not storage: storage = self.storage
+        save_to_storage = self.save_routines.get(storage)
+        if save_to_storage: save_to_storage()
+        else:
+            raise Exception('Where is no such storage %s!' % storage)
+        
+    # Save to Redis storage
+    def save_redis(self):
+        " Save object to redis storage"
+        if not self.id:
+            new_id = REDIS_CONN.incr(self.__class__.__name__.lower() + '_key')
+            self.id = new_id
+        self.save_redis_recursive(':'.join([self.collection_name, str(self.id)]), self.__instdict__)
+                
+    def save_redis_recursive(self, redis_key, valuedict):
+        " Recursive save instance dictionary to Redis storage"
+        for k in valuedict:
+            if type(valuedict[k]) != dict:
+                curr_key = ':'.join([redis_key, k])
+                REDIS_CONN.set(curr_key, valuedict[k])
+            else:
+                self.save_redis_recursive(':'.join([redis_key, k]), valuedict[k])
+    
+    # Save to Mongo
+    def save_mongo(self):
+        pass
+    
+    def get_structure(self):
+        classname = self.__class__.__name__
+        return self.__structure__.get(classname)
+    
+    def get_collection_name(self):
+        classname = self.__class__.__name__
+        return self.__collection__[classname]
+    
+    def get_values(self):
+        return copy.deepcopy(self.__instdict__)
+        
+    def load(self):
+        pass
     
 class Node(Value):
     " Use it for embedd objects to model "

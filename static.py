1542f76b7b549b00f9ab0aed5820e5c673fd6552
Niko Skrypnik <nskrypnik@gmail.com>
2011-06-30 20:19:24 +0300

    ongoing [#14925107] solved for Chrome

--- static.py
+++ static.py
@@ -3,6 +3,8 @@ import os
 from noodles.http import BaseResponse, Error404
 from config import STATIC_ROOT
 
+import logging
+
 # Mime types dictionary, contain pairs: key - file extansion,
 # value - mime type
 MIME_TYPES = {
@@ -32,7 +34,13 @@ MIME_TYPES = {
     # Thank you Jimmy
     }
 
+def toInt(val):
+    if val == '': return 0
+    return int(val)
+
 def index(request, path_info):
+    parital_response = False
+
     path_info = path_info.replace('%28', '(').replace('%29', ')').replace('%20', ' ')
     response = BaseResponse()
     # define a file extansion
@@ -42,14 +50,44 @@ def index(request, path_info):
     static_file_path = os.path.join(STATIC_ROOT, path_info)
     # Check if this path exists
     print 'static_file_path:', static_file_path
-    if not os.path.exists(static_file_path):
-        # TODO: return Error404 
+    if not os.path.exists(static_file_path): 
         error_msg = "<h1>Error 404</h1> No such file STATIC_ROOT/%s" % path_info
         return Error404(error_msg)
     # configure response
     static_file = open(static_file_path, 'rb') # Open file
-    response.body = static_file.read()
-    response.headerlist = [('Content-type', mime_type)]
+    # Here we try to handle Range parameter
+    
+    content_offset = 0
+    content_end = 0
+    request_range = request.headers.get('Range')
+    if request_range:
+        range_bytes = request_range.replace('bytes=', '')
+        logging.debug('================================== %s =========================================================' % path_info)
+        logging.debug('range_bytes %s' % range_bytes)
+        range_bytes = range_bytes.split('-')
+        if len(range_bytes) > 2: raise Exception('Wrong http Range parameter "%s"' % request_range)
+        content_offset = toInt(range_bytes[0])
+        content_end = toInt(range_bytes[1])
+        parital_response = True
+    
+    
+    static_content = static_file.read()        
+    if content_end <= 0 or content_end >= len(static_content): content_end = len(static_content) - 1
+    response.body = static_content[content_offset: content_end + 1]
+    
+
     response.charset = 'utf-8'
+    
+    if parital_response:
+        response.status = 206
+        response.headerlist = [('Content-type', mime_type), 
+        ('Content-Range', 'bytes %i-%i/%i' % (content_offset, content_end, len(static_content)))]
+        
+        logging.debug('Headers %s' % response.headerlist.__repr__())
+        logging.debug('------------------------------------------------------------------------------------------------------')
+    else:
+        response.headerlist = [('Content-type', mime_type)]
+
     # This seems to be clear, return this response object
+    
     return response

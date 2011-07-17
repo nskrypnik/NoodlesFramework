a1e2e7383e078354c6daa10063c252e91d9e0543
Niko Skrypnik <nskrypnik@gmail.com>
2011-07-17 19:28:22 +0300

    Django setting in djangoapp

--- controllers/djangoapp.py
+++ controllers/djangoapp.py
@@ -1,8 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
 
 try:
     from django.core.handlers.wsgi import WSGIHandler
 except:
-    raise Exception('Can\' import Django WSGIHandler. Have you installed django?')
-    
+    raise DjangoImportError('Can\' import Django WSGIHandler. Have you installed django?')
+
+try:
+    from config import DJANGO_SETTINGS
+except:
+    raise DjangoSettingsError('Django settings module is undefined')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = DJANGO_SETTINGS
+
 def run_django(request):
     return WSGIHandler()
+
+class DjangoImportError(Exception):
+    pass
+
+class DjangoSettingsError(Exception):
+    pass

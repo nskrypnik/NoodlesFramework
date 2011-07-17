43deca4a7843f3d004d80dbbc1ac514cb0a1ec93
Niko Skrypnik <nskrypnik@gmail.com>
2011-07-17 19:34:14 +0300

    some cosmetic changes to manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-import sys,commands,os
-mydir = os.path.dirname(sys.argv[0])
-tpldir = os.path.join(mydir,'project_template')
+import sys, commands, os
+
+current_dir = os.path.dirname(sys.argv[0])
+template_dir = os.path.join(current_dir,'project_template')
 op = sys.argv[1]
 if op=='init':
-    cmd = 'cp -r -i %s/* .'%(tpldir)
-    st,op = commands.getstatusoutput(cmd)
-    assert st==0,"%s returned %s"%(cmd,st)
+    cmd = 'cp -r -i %s/* .' % (template_dir)
+    st, op = commands.getstatusoutput(cmd)
+    assert st == 0, "%s returned %s" % (cmd,st)

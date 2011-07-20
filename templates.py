6e5423a00edd7acac2d3a1d58425d532b5d39f73
Niko Skrypnik <nskrypnik@gmail.com>
2011-07-20 14:43:42 +0300

    Add contextManager to manage application context for template rendering

--- templates.py
+++ templates.py
@@ -4,23 +4,62 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 from noodles.http import Response
 from config import TEMPLATE_DIRS, MAKO_TMP_DIR
+
+import config
 import os
 
 # Specify application lookup
 appLookup = TemplateLookup(directories=TEMPLATE_DIRS,
                 module_directory=MAKO_TMP_DIR, output_encoding='utf-8', input_encoding='utf-8')
 
+class ContextManager(object):
+    """ 
+        ContextManager class. Use contextManager object to manage your application context 
+    """
+    def __new__(cls):
+        if '_inst' not in vars(cls):
+            cls._inst = object.__new__(cls)
+            return cls._inst
+    
+    def __init__(self):
+        self.context_processors = []
+        self.context_constants = {}
+        
+    def add_processor(self, func):
+        """
+             Add context processor function to generate general context.
+             Func function must return dictionary of context variables 
+        """
+        self.context_processors.append(func)
+
+    def add_constants(self, constants):
+        " Add dictionary of constant template variables "
+        self.context_constants.update(constants)
+    
+    def update_context(self, context, request = None):
+        # Add request and config objects to context
+        if request:
+            context['request'] = request
+        context['config'] = config
+        
+        # Update context by constants
+        context.update(self.context_constants)
+        
+        # Update context by context processors
+        for processor in self.context_processors:
+            c = processor(request)
+            context.update(c)
+            
+contextManager = ContextManager()
+    
+
 def render_to_response(templatename, context, request = None):
-    if request:
-        context['request'] = request
-    template = appLookup.get_template(templatename)
-    rendered_page = template.render(**context)
+    rendered_page = render_to_string(templatename, context, request)
     return Response(rendered_page)
 
 def render_to_string(templatename, context, request = None):
     " Just renders template to string "
-    if request:
-        context['request'] = request
+    contextManager.update_context(context, request)
     template = appLookup.get_template(templatename)
     rendered_page = template.render(**context)
     return rendered_page
@@ -39,10 +78,8 @@ def render_to(templatename):
             # Get context from the handler function
             context = func(**kwargs)
             # Add some extra values to context
-            context['request'] = kwargs['request'] # while it's enough :)
-            # Get a tamplate object by the template name
-            template = appLookup.get_template(templatename)
-            rendered_page = template.render(**context)
+            request = kwargs['request'] # while it's enough :)
+            rendered_page = render_to_string(templatename, context, request)
             return Response(rendered_page)
         return wrapper
     return renderer
